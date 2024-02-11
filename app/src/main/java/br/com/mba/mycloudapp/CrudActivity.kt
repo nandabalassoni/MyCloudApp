3fdac4895c8cea71a2fb20f0f0689de2d453e453
@bruno.santos.ciandt <bruno.santos_ciandt@prestador.globo>
2024-02-11 01:26:31 -0300

    Added Crud view.

--- app/src/main/java/br/com/mba/mycloudapp/CrudActivity.kt
+++ app/src/main/java/br/com/mba/mycloudapp/CrudActivity.kt
@@ -1,11 +1,10 @@
 package br.com.mba.mycloudapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class CrudActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
     }
 }
--- app/src/main/java/br/com/mba/mycloudapp/SignInActivity.kt
+++ app/src/main/java/br/com/mba/mycloudapp/SignInActivity.kt
@@ -1,11 +1,38 @@
 package br.com.mba.mycloudapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import br.com.mba.mycloudapp.databinding.SignInBinding
 
 class SignInActivity: AppCompatActivity() {
+
+    private lateinit var binding: SignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sign_in)
+        binding = SignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bindings()
+    }
+
+    private fun bindings() {
+        binding.btnSignInRegister.setOnClickListener {
+            if(binding.editTextSignInUser.text.toString().isEmpty()) {
+                Toast.makeText(this, "Informe um usuário", Toast.LENGTH_LONG).show()
+            } else if(binding.editTextSignInPass.text.toString().isEmpty()) {
+                Toast.makeText(this, "Informe a senha", Toast.LENGTH_LONG).show()
+            } else if(binding.editTextSignInConfirmPass.text.toString().isEmpty()) {
+                Toast.makeText(this, "Confirme a senha digitando novamente", Toast.LENGTH_LONG).show()
+            } else if(!binding.editTextSignInPass.text.toString().equals(binding.editTextSignInConfirmPass.text.toString())) {
+                Toast.makeText(this, "A senha e a confirmação da senha não são iguais", Toast.LENGTH_LONG).show()
+            } else {
+                val nextScreen = Intent(this, CrudActivity::class.java)
+                startActivity(nextScreen)
+                finish()
+            }
+        }
     }
 }
