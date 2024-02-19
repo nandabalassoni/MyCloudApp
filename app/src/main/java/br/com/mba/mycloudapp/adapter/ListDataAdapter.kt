23adfe6de53f0024fa94ee3383cef85cf2c32e50
nandabalassoni <mariafernandabalassoni@hotmail.com>
2024-02-19 03:05:13 -0300

    add item id and configurind delete by id

--- app/src/main/java/br/com/mba/mycloudapp/adapter/ListDataAdapter.kt
+++ app/src/main/java/br/com/mba/mycloudapp/adapter/ListDataAdapter.kt
@@ -4,13 +4,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import br.com.mba.mycloudapp.databinding.ItemListBinding
 import br.com.mba.mycloudapp.model.ClothingItem
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ListDataAdapter(
     private var itemList: MutableList<ClothingItem>
 ) : RecyclerView.Adapter<ListDataAdapter.ItemListViewHolder>() {
+
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -25,9 +29,20 @@ class ListDataAdapter(
         holder.tvItem.text = item.name
         holder.colorItem.text = item.color
         holder.itemPrice.text = item.price.toString()
+
         holder.btnDelete.setOnClickListener {
+            val itemToRemove = itemList[position]
             itemList.removeAt(position)
             notifyItemRemoved(position)
+
+            // Aqui é feita a exclusão do item no Firestore. nesse caso apenas de novos itens cadastrados, pois é gerado um id novo para cada um.
+            db.collection("clothing_items").document(itemToRemove.id).delete()
+                .addOnSuccessListener {
+                    Toast.makeText(holder.itemView.context, "Item excluído com sucesso!", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(holder.itemView.context, "Erro ao excluir item", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
